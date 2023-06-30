4d1e5b1f27016bd4eb12ebb19321304603d246fe
xing <xing_kai2022@163.com>
2023-06-30 15:22:28 +0800

    XA-Cloud3.0:重新设计展示画面，调整模块分布，引入全新地图

--- src/views/xanadu/DataScreen/bottomLeft.vue
+++ src/views/xanadu/DataScreen/bottomLeft.vue
@@ -1,9 +1,7 @@
-
 <template>
   <div >
-    <div ref="chart1" style="margin-top: 20px;width:10rem;height:4rem"></div>
+    <div ref="chart1" style="margin-top: 20px;width:11rem;height:5rem"></div>
   </div>
-
 </template>
 
 <script>
@@ -11,11 +9,12 @@
 export default {
   name:'bottomLeft',
   data() {
-    return{}
+    return{
+      lineData:[],
+    }
   },
   mounted() {
     this.getEchartData1()
-
   },
   methods: {
     getEchartData1() {
@@ -24,24 +23,27 @@ export default {
         const myChart = this.$echarts.init(chart1)
         const option = {
           title: {
-            text: '各商品出货量'
+            text: '各种订单数量统计',
+            textStyle: {
+              color: 'white'
+            },
           },
           tooltip: {
             trigger: 'axis'
           },
           legend: {
-            data: ['阿伟罗软蛋', '矿泉水', '阿伟李硬蛋', '草莓', '风扇']
+            data:this.lineData.name,
+            type: 'scroll',
+            right:'10',
+            textStyle: {
+              color: '#'
+            },
           },
           grid: {
             left: '3%',
             right: '4%',
             bottom: '3%',
             containLabel: true
-          },
-          toolbox: {
-            feature: {
-              saveAsImage: {}
-            }
           },
           xAxis: {
             type: 'category',
@@ -51,55 +53,48 @@ export default {
           yAxis: {
             type: 'value'
           },
-          series: [
-            {
-              name: '阿伟罗软蛋',
-              type: 'line',
-              stack: 'Total',
-              data: [120, 132, 101, 134, 90, 230, 210,120, 132, 101, 134, 90, 230]
-            },
-            {
-              name: '矿泉水',
-              type: 'line',
-              stack: 'Total',
-              data: [220, 182, 191, 234, 290, 330, 310,220, 182, 191, 234, 290, 330]
-            },
-            {
-              name: '阿伟李硬蛋',
-              type: 'line',
-              stack: 'Total',
-              data: [150, 232, 201, 154, 190, 330, 410,150, 232, 201, 154, 190, 330]
-            },
-            {
-              name: '草莓',
-              type: 'line',
-              stack: 'Total',
-              data: [320, 332, 301, 334, 390, 330, 320,320, 332, 301, 334, 390, 330]
-            },
-            {
-              name: '风扇',
-              type: 'line',
-              stack: 'Total',
-              data: [820, 932, 901, 934, 1290, 1330, 1320,820, 932, 901, 934, 1290, 1330]
-            }
-          ]
+          series:this.lineData
 
         }
         myChart.setOption(option)
-        window.addEventListener("resize", function() {
-          myChart.resize()
-        })
       }
-      this.$on('hook:destroyed',()=>{
-        window.removeEventListener("resize", function() {
-          myChart.resize();
-        });
-      })
+
     },
 
   },
-  watch: {},
   created() {
+    let list =  [
+       {
+         name: '阿伟罗软蛋',
+         type: 'line',
+         stack: 'Total',
+         data: [120, 132, 101, 134, 90, 230, 210,120, 132, 101, 134, 90, 230]
+       },
+       {
+         name: '阿伟徐傻蛋',
+         type: 'line',
+         stack: 'Total',
+         data: [220, 182, 191, 234, 290, 330, 310,220, 182, 191, 234, 290, 330]
+       },
+       {
+         name: '阿伟李硬蛋',
+         type: 'line',
+         stack: 'Total',
+         data: [150, 232, 201, 154, 190, 330, 410,150, 232, 201, 154, 190, 330]
+       },
+       {
+         name: '李志鹏滚蛋',
+         type: 'line',
+         stack: 'Total',
+         data: [320, 332, 301, 334, 390, 330, 320,320, 332, 301, 334, 390, 330]
+       }
+     ]
+    for (let i = 0;i < list.length;i++)
+      this.lineData.push(list[i])
   }
 }
 </script>
+
+<style scoped>
+
+</style>
--- src/views/xanadu/DataScreen/centerLeft.vue
+++ src/views/xanadu/DataScreen/centerLeft.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="centerLeft">
-    <div class="bg-color-black">
+    <div class="cl">
       <div class="d-flex pt-2 pl-2">
-        <span style="color:#5cd9e8"></span>
-        <span class="fs-xl text mx-2">各库出货量统计</span>
+        <span style="color:#5cd9e8">
+        </span>
+        <span class="fs-xl text mx-2">订购排行榜</span>
       </div>
-      <div class="d-flex jc-center body-box" style=" margin-top: 10px;">
-        <dv-capsule-chart :config="config" style="width: 4rem;height:3rem" />
+      <div class="d-flex jc-center body-box" style=" margin-top: 10px;margin-left: 20px">
+        <dv-scroll-ranking-board :config="config" style="width: 6rem;height:3rem" />
       </div>
     </div>
   </div>
@@ -17,52 +18,51 @@ export default {
   name: 'centerLeft',
   data() {
     return{
+      list:[
+        {
+          name: "李嘉伟",
+          value: 89
+        },
+        {
+          name: "徐高松",
+          value: 4
+        },
+        {
+          name: "邢凯",
+          value: 100
+        },
+        {
+          name: "阿伟罗",
+          value: 7
+        },
+        {
+          name: "李志鹏",
+          value: 66
+        },
+        {
+          name: "李志鹅",
+          value: 77
+        },
+        {
+          name: "刘益先",
+          value: 56
+        }
+      ],
       config:{
-        data:[
-          {
-            name: "南阳",
-            value: 167
-          },
-          {
-            name: "周口",
-            value: 67
-          },
-          {
-            name: "漯河",
-            value: 123
-          },
-          {
-            name: "郑州",
-            value: 55
-          },
-          {
-            name: "西峡",
-            value: 98
-          },
-        ]
+        data:[]
       }
     }
+  },
+  mounted() {
+    for (let i = 0;i < this.list.length;i++)
+      this.config.data.push(this.list[i])
   }
 }
 </script>
 
-<style lang="scss">
-#centerLeft {
-  padding: 0.0625rem;
-  height: 3.5rem;
-  min-width: 3.75rem;
-  border-radius: 0.0625rem;
-  .bg-color-black {
-    padding: 0.0625rem;
-    height: 5.0625rem;
-    border-radius: 0.125rem;
-  }
-  .text {
-    color: #c3cbde;
-  }
-  .body-box {
-    border-radius: 0.125rem;
-    overflow: hidden;
-  }
+<style scoped>
+.cl{
+  width: 7rem;
+  height: 3.7rem;
 }
 </style>
