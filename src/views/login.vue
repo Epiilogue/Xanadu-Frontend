4852fecf9936b43dd6d5225893819b4ebe7e15d6
GaosongXu <shimaring@qq.com>
2023-07-13 15:46:02 +0800

    登录页简单修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -2,43 +2,41 @@
   <div class="login">
 
     <el-tabs v-model="activeName" type="card" class="card">
-      <el-tab-pane label="账户登录" name="first">
-
+      <el-tab-pane label="账户登录" name="first" >
         <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
           <h3 class="title">Xanadu物流运输管理系统</h3>
-          <el-form-item prop="username">
+          <el-form-item prop="username" label="用户名">
             <el-input
               v-model="loginForm.username"
               type="text"
-              auto-complete="off"
-              placeholder="账号"
+              auto-complete="true"
+              style="width: 80%"
             >
-              <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon"/>
+
             </el-input>
           </el-form-item>
-          <el-form-item prop="password">
+          <el-form-item prop="password" label="密码">
             <el-input
               v-model="loginForm.password"
               type="password"
-              auto-complete="off"
-              placeholder="密码"
+              auto-complete="true"
               @keyup.enter.native="handleLogin"
+              style="width: 80%;margin-left: 15px;"
             >
-              <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon"/>
             </el-input>
           </el-form-item>
 
-          <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
           <el-form-item style="width:100%;">
             <el-button
               :loading="loading"
               size="medium"
               type="primary"
               style="width:100%;"
+              plain
               @click.native.prevent="handleLogin"
             >
-              <span v-if="!loading">登 录</span>
-              <span v-else>登 录 中...</span>
+              <span v-if="!loading">Login</span>
+              <span v-else>Waiting...</span>
             </el-button>
           </el-form-item>
         </el-form>
@@ -65,10 +63,11 @@
                 size="medium"
                 type="primary"
                 style="width:100%;"
+                plain
                 @click.native.prevent="handleLoginByEmail(emailForm)"
               >
-                <span v-if="!loading">登 录</span>
-                <span v-else>登 录 中...</span>
+                <span v-if="!loading">Login</span>
+                <span v-else>Waiting...</span>
               </el-button>
             </el-form-item>
           </el-form-item>
@@ -83,9 +82,9 @@
 </template>
 
 <script>
-import { getCode, getCodeImg } from '@/api/login'
+import { getCode } from '@/api/login'
 import Cookies from 'js-cookie'
-import { encrypt, decrypt } from '@/utils/jsencrypt'
+import { decrypt, encrypt } from '@/utils/jsencrypt'
 
 export default {
   name: 'Login',
@@ -249,13 +248,13 @@ export default {
 }
 
 .card {
+
   margin-left: 65%;
   margin-top: -5%;
   border-radius: 6px;
-  background: #FFFFFF;
+  background-color: rgba(255, 255, 255, 1);
   width: 500px;
   padding: 25px 25px 15px 25px;
-
   .el-input {
     height: 45px;
 
@@ -272,12 +271,7 @@ export default {
 }
 
 .login-form {
-  //margin-left: 65%;
-  //margin-top: -5%;
-  //border-radius: 6px;
-  //background: #FFFFFF;
-  //width: 400px;
-  //padding: 2px 25px 5px 25px;
+
   .el-input {
     height: 45px;
 
